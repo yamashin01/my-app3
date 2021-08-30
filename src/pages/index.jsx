1f3a493b10f9e3c1d30a88956996869f026e8694
s-yamada2 <s-yamada@future-tech-association.org>
2021-08-30 09:56:19 +0900

    ✨ 説明用useEffectの導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import { Footer } from 'src/components/Footer'
 import { Header } from 'src/components/Header'
 import Main from 'src/components/Main'
@@ -11,13 +11,22 @@ import styles from '../styles/Home.module.css'
 // }
 
 export default function Home() {
-  // const foo = 1;
-  // const handleClick = useCallback ((e) => {
-  //   console.log(e.target.href);
-  //   e.preventDefault();
-  //   alert(foo);
-  // }, []);
-  
+  const foo = 1;
+  const handleClick = useCallback ((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    }
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
