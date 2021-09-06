bb5ee569e83010d9e9a689e517c7cc1938e633d6
s-yamada2 <s-yamada@future-tech-association.org>
2021-09-06 09:33:37 +0900

    ✨ カウンターの導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { useCallback, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Footer } from 'src/components/Footer'
 import { Header } from 'src/components/Header'
 import Main from 'src/components/Main'
@@ -11,18 +11,16 @@ import styles from '../styles/Home.module.css'
 // }
 
 export default function Home() {
-  const foo = 1;
-  const handleClick = useCallback ((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const [count, setCount] = useState(1);
+
+  const handleClick = (e) => {
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+  };
 
   useEffect(() => {
-    console.log("マウント時");
     document.body.style.backgroundColor = "lightblue";
     return () => {
-      console.log("アンマウント時");
       document.body.style.backgroundColor = "";
     }
   }, []);
@@ -35,12 +33,12 @@ export default function Home() {
       </Head>
 
       <Header />
-      {/* <a
-       href="/about"
-      //  onClick = {handleClick}
+      <h1>{count}</h1>
+      <button
+       onClick = {handleClick}
        >
         ボタン
-      </a> */}
+      </button>
 
       <Main page="index"/>
 
