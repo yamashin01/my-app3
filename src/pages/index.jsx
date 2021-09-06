8559a64ec5cd22011543afb1e6394af5d36225be
s-yamada2 <s-yamada@future-tech-association.org>
2021-09-06 09:53:16 +0900

    ⚡️ useCallback, useEffectの第２引数の説明

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,22 +1,18 @@
 import Head from 'next/head'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { Footer } from 'src/components/Footer'
 import { Header } from 'src/components/Header'
 import Main from 'src/components/Main'
 import styles from '../styles/Home.module.css'
 
-// const handleClick = (e) => {
-//   console.log(e.target.href);
-//   e.preventDefault();
-// }
-
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    setCount(count => count + 1);
-    setCount(count => count + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount(count => count + 1);
+    }
+  }, [count]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
