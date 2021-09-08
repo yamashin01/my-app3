f04ae3207ab16b1d3e4887f1ab739f49282e4543
s-yamada2 <s-yamada@future-tech-association.org>
2021-09-08 09:43:35 +0900

    ✨ useStateで文字列と真偽値を追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,12 +7,26 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
       setCount(count => count + 1);
     }
   }, [count]);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内");
+      return;
+    }
+      setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow)=> !isShow)
+  })
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -29,12 +43,16 @@ export default function Home() {
       </Head>
 
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
+      <button onClick= {handleDisplay}>
+        {isShow ? "非表示" : "表示"}
+      </button>
       <button
        onClick = {handleClick}
        >
         ボタン
       </button>
+      <input type="text" value={text} onChange={handleChange}/>
 
       <Main page="index"/>
 
