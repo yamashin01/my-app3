66f9cc9ed245c19c10e8133c2650c97707db0046
s-yamada2 <s-yamada@future-tech-association.org>
2021-09-13 09:54:08 +0900

    ✨ コンポーネント間のStateのリフトアップ

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -5,10 +5,10 @@ export function Headline(props) {
   return (
     <div>
       <h1 className={styles.title}>{props.page} page</h1>
-
       <p className={styles.description}>
-        Get started by editing {props.children}
+        アイテムの数は {props.children}個です。
       </p>
+      <button onClick={props.handleReduce}>減らす</button>
     </div>
     )
 }
