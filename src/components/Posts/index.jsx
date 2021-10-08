bc3119d6f02e2c268fa82d09eebf138b76f2641c
s-yamada2 <s-yamada@future-tech-association.org>
2021-10-08 09:43:19 +0900

    🚚 ファイルのルーティング

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,4 +1,5 @@
 import { usePosts } from "src/hooks/usePosts";
+import Link from "next/link";
 
 export const Posts = () => {
   const { data, error , isLoading, isEmpty } = usePosts();
@@ -15,9 +16,13 @@ export const Posts = () => {
 
   return (
       <ol>
-          {data.map(post => {
+          {data.map((post) => {
               return (
-                  <li key={post.id}>{post.title}</li>
+                  <li key={post.id}>
+                      <Link href ={`/post/${post.id}`}>
+                          <a>{post.title}</a>
+                      </Link>
+                  </li>
               )
           })}
       </ol>
--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { Footer } from 'src/components/Footer'
 import { Header } from 'src/components/Header'
 import Main from 'src/components/Main'
-import styles from '../styles/Home.module.css'
+import styles from 'src/styles/Home.module.css'
 
 const About = (props) => {
 
