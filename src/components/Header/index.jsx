b01caa77fc61cdf5b33bc995783ba48de8694307
s-yamada2 <s-yamada@future-tech-association.org>
2021-10-22 09:18:31 +0900

    🐛 パスの変更し忘れ修正

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 import styles from "./Header.module.css";
 
 const NAV_ITEMS = [
+  {href: "/", label: "Index"},
   {href: "/posts", label: "Posts"},
-  {href: "/about", label: "About"},
 ]
 export const Header =() => {
   return (
